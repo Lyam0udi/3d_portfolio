ced0f6bbd723fe1ab4c5f9fb518690e689112f68
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-11-24 20:16:28 +0100

    adjust Island model for ScreenSize

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,24 @@ import Island from '../models/Island'
       </div> */}
 
 const Home = () => {
+
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -0.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if(window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandrotation] = adjustIslandForScreenSize();
+
+
   return (
     <section className='w-full h-screen relative'>
       <Canvas className='w-full h-screen bg-transparent'
@@ -18,7 +36,11 @@ const Home = () => {
             <spotLight />
             <hemisphereLight />
 
-            <Island />
+            <Island 
+              position = {islandPosition} 
+              scale = {islandScale} 
+              rotation = {islandrotation}
+            />
         </Suspense>
       </Canvas>
     </section>
