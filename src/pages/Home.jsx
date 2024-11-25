8efc345942a48e091d6a11b3644fa85a20ce758a
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-11-25 14:30:27 +0100

    implement Bird and Plane 3D model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,8 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
 import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 // import { Sky } from '@react-three/drei'
 
 {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
@@ -39,12 +41,14 @@ const Home = () => {
             {/* <spotLight /> */}
             <hemisphereLight skyColor ="#b1e1ff" groundColor="#000000" intensity={1} />
 
+            <Bird />
             <Sky />
             <Island 
               position = {islandPosition} 
               scale = {islandScale} 
               rotation = {islandrotation}
             />
+            <Plane/>
         </Suspense>
       </Canvas>
     </section>
