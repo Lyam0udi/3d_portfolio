6f743161c446e50ffe1df8f192edd7273518f459
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-11-26 16:47:14 +0100

    animate the 3D Bird model

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,12 +1,22 @@
-import { useGLTF } from '@react-three/drei';
-import React from 'react'
+import { useAnimations, useGLTF } from '@react-three/drei';
+import { useRef, useEffect } from 'react'
 import birdScene from '../assets/3d/bird.glb';
 
 const Bird = () => {
+    const birdRef = useRef();
     const { scene, animations } = useGLTF(birdScene);
+    const { actions } = useAnimations(animations, birdRef);
+
+    useEffect (() => {
+        actions["Take 001"].play();
+    },[]);
 
     return (
-        <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
+        <mesh 
+            position={[-5, 2, 1]} 
+            scale={[0.003, 0.003, 0.003]}
+            ref={birdRef}
+        >
             <primitive object={scene}/>
         </mesh>
     )
