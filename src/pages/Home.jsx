b297406ee2c5fe8a8a84d71c79ed7f74389b76ab
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-11-27 18:56:45 +0100

    Initialize the first popup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,9 +7,7 @@ import Bird from '../models/Bird'
 import Plane from '../models/Plane'
 // import { Sky } from '@react-three/drei'
 
-{/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        POPUP
-      </div> */}
+
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -51,6 +49,11 @@ const Home = () => {
 
   return (
     <section className='w-full h-screen relative'>
+
+    <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        POPUP
+      </div>
+
       <Canvas
         className={`w-full h-screen bg-transparent ${
           isRotating ? "cursor-grabbing" : "cursor-grab"
