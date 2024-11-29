8bf67ec4f3a72a1adb420a85a6e99b22f2c3f7db
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-11-29 21:40:23 +0100

    Complete home centent and improve popups's style

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { arrow } from '../assets/icons';
 
 const InfoBox = ({ text, link, btnText }) => (
     <div className='info-box'>
-        {text}
-        <Link to={link}>
+        <p className='font-medium sm:text-xl text-center'>{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
             {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain"/>
         </Link>
 
     </div>
@@ -27,10 +29,18 @@ const renderContent = {
         />
     ),
     3: (
-        <h1>3</h1>
+        <InfoBox 
+            text = "Led multiple projects to success over the years. Curious about the impact?"
+            link="/projects"
+            btnText="Visit my portfolio"
+        />
     ),
     4: (
-        <h1>4</h1>
+        <InfoBox 
+            text = "Need a project done or looging for a dev? I'm just a few keystrokes away"
+            link="/contact"
+            btnText="Let's talk!"
+        />
     ),
 }
 
