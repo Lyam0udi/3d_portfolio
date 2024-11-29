237a3707b76e3449f734adec65bd808bb573018a
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-11-29 22:23:48 +0100

    implement the first input in the contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Contact = () => {
+
+  const [form, setform] = useState({ name: '', email: '', message:''})
+  const handleChange = () => {};
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+
+
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
       <div className='flex-1 min-w-[50%] flex flex-col'>
@@ -10,8 +18,18 @@ const Contact = () => {
         >
           <label className='text-black-500 font-semibold'>
             Name
+            <input 
+            type="text"
+            name="name"
+            className="input"
+            placeholder="Name"
+            required
+            value={form.name}
+            onChange={handleChange}
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          />
           </label>
-
         </form>
       </div>
     </section>
