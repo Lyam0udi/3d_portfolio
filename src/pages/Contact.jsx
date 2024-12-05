c0f94a0e0a5725bef6e0d9b4e612350377fc7832
Lyam0udi <lyamoudi.ilias77@gmail.com>
2024-12-05 11:15:34 +0100

    Configure the fox's running animation to stop And Initializing Alerts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,12 +3,16 @@ import emailjs from '@emailjs/browser';
 import { Canvas } from '@react-three/fiber';
 import Fox from '../models/Fox';
 import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message:''});
   const [isLoading, setIsLoading] = useState(false);
-  const [currentAnimation, setCurrentAnimation] = useState('idle')
+  const [currentAnimation, setCurrentAnimation] = useState('idle');
+
+  const { alert, showAlert, hideAlert } = useAlert();
   
   const handleChange = (e) => {
     setForm({ ...form,[e.target.name]: e.target.value})
@@ -37,17 +41,26 @@ const Contact = () => {
     .then (() => {
       setIsLoading(false);
       // Show success alert and hide it
-      setForm({name:'', email:'', message: ''});
+      showAlert({ show: true, text: 'Message sent successfully !', type: 'success'});
+      //setForm({name:'', email:'', message: ''});
+      setTimeout(() => {
+        hideAlert();
+        setCurrentAnimation('idle');
+        setForm({ name: '', email: '', message: ''})
+      }), [3000];
     }).catch((error) =>{
       setIsLoading(false);
       setCurrentAnimation('idle');
       console.log(error);
       // show error alert message
+      showAlert({ show: true, text: 'Message not received yet !', type: 'danger'});
     })
   };
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert {...alert}/>}
+      <Alert {...alert}/>
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
         <form
